591fbd1f6ebf2a49b1cc895000465d6739e02d3d
OmarnLujan <127269393+OmarnLujan@users.noreply.github.com>
2023-09-28 14:06:06 -0300

    cambios

--- porfolio/src/app/page.tsx
+++ porfolio/src/app/page.tsx
@@ -2,12 +2,12 @@
 export default function Home() {
   return (
     <div>
-      <div className="m-40 flex flex-col justify-center relative items-center text-center">
-        <h3 className=" justify-center items-center text-center">
-          Buenas soy Omar Nazareno Lujan, 
+      <div className="m-40 flex  flex-col justify-center relative items-center text-center">
+        <h3 className="font-mono w-3/4 text-2xl justify-center items-center text-center">
+          Buenas soy Omar Nazareno Lujan, me llaman Nazareno me gradue de Tecnico Informatico, estudie dos años Ingenieria Informatica, gracias a ese tiempo decidi enfocarme en estudiar programacion.
         </h3>
       </div>
-      
+     
     </div>
   );
 }
